b6c15633f63a3e708a5a908be47a6341d971bbe7
Mateusz Rytwiński <mateuszrytwinski@onet.pl>
2021-01-27 14:33:07 +0100

    added List in ResultsActivity and changed colors of icons

--- app/src/main/java/com/example/androidchampionsleague/ResultsActivity.java
+++ app/src/main/java/com/example/androidchampionsleague/ResultsActivity.java
@@ -2,18 +2,47 @@ package com.example.androidchampionsleague;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class ResultsActivity extends RootActivity {
+
+    ListView listView;
+    ArrayList<String> stagesList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
         drawerLayout = findViewById(R.id.drawer_layout);
+
+        //listView = findViewById(R.id.list_view);
+
+        initStagesList();
     }
 
     @Override
-    public void ClickResults(View view){
+    public void ClickResults(View view) {
         recreate();
+    }
+
+    private void initStagesList(){
+        stagesList.add(getString(R.string.group_stage));
+        stagesList.add(getString(R.string.quarterfinal_stage));
+        stagesList.add(getString(R.string.semifinal_stage));
+        stagesList.add(getString(R.string.final_stage));
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        ResultsRecyclerViewAdapter adapter = new ResultsRecyclerViewAdapter(this, stagesList);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ app/src/main/java/com/example/androidchampionsleague/ResultsRecyclerViewAdapter.java
@@ -0,0 +1,61 @@
+package com.example.androidchampionsleague;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ResultsRecyclerViewAdapter extends RecyclerView.Adapter<ResultsRecyclerViewAdapter.ViewHolder>{
+
+    private ArrayList<String> mStages;
+    private Context mContext;
+
+    public ResultsRecyclerViewAdapter(Context context, ArrayList<String> stages ) {
+        mStages = stages;
+        mContext = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.results_list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+
+        holder.stageName.setText(mStages.get(position));
+
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(mContext, mStages.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mStages.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView stageName;
+        CardView parentLayout;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            stageName = itemView.findViewById(R.id.stage_name);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+        }
+    }
+}
+

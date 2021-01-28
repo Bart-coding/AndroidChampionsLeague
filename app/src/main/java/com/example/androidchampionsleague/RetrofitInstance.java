2c1ec40d1cf5c4c724743ab8cf25384155b3058e
Bart-coding <bartekpoczta54@gmail.com>
2021-01-28 14:13:58 +0100

    added header declaration for creating Retrofit instance

--- app/src/main/java/com/example/androidchampionsleague/RetrofitInstance.java
+++ app/src/main/java/com/example/androidchampionsleague/RetrofitInstance.java
@@ -1,6 +1,11 @@
 package com.example.androidchampionsleague;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -10,14 +15,26 @@ public class RetrofitInstance {
     public static final String FOOTBALL_API_URL = "https://api.football-data.org/";
 
     public static Retrofit getRetrofitInstance() {
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-
-        OkHttpClient client = new OkHttpClient.Builder()
-                .addInterceptor(interceptor)
-                .build();
 
         if (retrofit == null) {
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(new Interceptor() {
+                        @Override
+                        public okhttp3.Response intercept(Chain chain) throws IOException {
+                            Request request = chain.request().newBuilder()
+                                    .addHeader("X-Auth-Token", "fc23b99dfbe1435bae465f0b137fa7ca")
+                                    .addHeader("Cache-Control", "max-age=640000")
+                                    .build();
+                            return chain.proceed(request);
+                        }
+                    })
+                    .addInterceptor(interceptor)
+                    .build();
+
+
             retrofit = new Retrofit.Builder()
                     .baseUrl(FOOTBALL_API_URL)
                     .addConverterFactory(GsonConverterFactory.create())
--- app/src/main/java/com/example/androidchampionsleague/ScorersService.java
+++ app/src/main/java/com/example/androidchampionsleague/ScorersService.java
@@ -5,10 +5,10 @@ import retrofit2.http.GET;
 import retrofit2.http.Headers;
 
 public interface ScorersService {
-    @Headers({
+    /*@Headers({
             "X-Auth-Token: fc23b99dfbe1435bae465f0b137fa7ca",
             "Cache-Control: max-age=640000"
-    })
+    })*/
     @GET("v2/competitions/CL/scorers?season=2019")
     Call<Object> getScorers();
 }

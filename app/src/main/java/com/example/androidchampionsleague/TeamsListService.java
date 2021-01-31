d7ed2284b2c008d15c6cca729e49297acd55a033
Bart-coding <bartekpoczta54@gmail.com>
2021-01-31 01:27:55 +0100

    changed TeamsListService for getting teams from 2019

--- app/src/main/java/com/example/androidchampionsleague/TeamsListService.java
+++ app/src/main/java/com/example/androidchampionsleague/TeamsListService.java
@@ -4,6 +4,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface TeamsListService {
-    @GET("v2/competitions/CL/teams?stage=GROUP_STAGE")
+    @GET("v2/competitions/CL/teams?season=2019&stage=GROUP_STAGE")
     Call<Object> getTeams();
 }
